7b473673b7628b965e991ef82dfdd204cb582eb3
Gabo <gaabf@hotmail.com>
2022-10-14 15:56:48 -0500

    UPDATE: fetch call and initial functions created

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-fetch('http://127.0.0.1:3000/products')
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.log(err))
-
-
-//Selector
+//Selectors
 const mainContainer = document.querySelector('.main');
 
+let url = 'http://127.0.0.1:3000/products'
+
+
+async function CallAPI(url){
+  const response = await fetch(url);
+  let data = await response.json()
+  return data
+}
+
+
+
+function renderProduct(container, array){
+  array.forEach(product => {
+    const ProductDiv = document.createElement('div');
+    ProductDiv.classList.add('product');
+    ProductDiv.innerText = product.name
+    container.appendChild(ProductDiv);
+  })
+}
+
+window.addEventListener('load', () => {
+  CallAPI(url).then(data => renderProduct(mainContainer, data))  
+})
