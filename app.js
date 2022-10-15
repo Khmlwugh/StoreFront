d1114671eb2ecffbee915d8508ed13314fabfc5e
Gabo <gaabf@hotmail.com>
2022-10-14 21:34:45 -0500

    ADD: fetch call and store product layout added

--- app.js
+++ app.js
@@ -14,10 +14,41 @@ async function CallAPI(url){
 
 function renderProduct(container, array){
   array.forEach(product => {
-    const ProductDiv = document.createElement('div');
-    ProductDiv.classList.add('product');
-    ProductDiv.innerText = product.name
-    container.appendChild(ProductDiv);
+
+    //Elements creation
+    const ProductContainer = document.createElement('div');
+    const ProductName = document.createElement('div');
+    const ProductImage = document.createElement('img')
+    const ProductPriceContainer = document.createElement('div');
+    const ProductPrice = document.createElement('div');
+    const PurchaseIcon = document.createElement('i')
+
+
+    //Adding classes and attributes to elements
+    ProductContainer.classList.add('product-container')
+    ProductImage.src = product.url_image;
+    ProductImage.classList.add('product-image');
+    ProductName.classList.add('product-name');
+    ProductName.innerText = product.name;
+    
+    ProductPriceContainer.classList.add('product-price-container')
+    ProductPrice.classList.add('product-price')
+    PurchaseIcon.classList.add('purchase-ícon')
+    PurchaseIcon.classList.add('ri-shopping-cart-line')
+    ProductPrice.innerText = '$' + product.price;
+
+    //Appending children to the Product Container
+    ProductContainer.appendChild(ProductImage);
+    ProductContainer.appendChild(ProductName);
+    ProductContainer.appendChild(ProductPriceContainer)
+
+    //
+
+    ProductPriceContainer.appendChild(ProductPrice)
+    ProductPriceContainer.appendChild(PurchaseIcon)
+
+    //Appending Product Container to Main Container
+    container.appendChild(ProductContainer);
   })
 }
 
